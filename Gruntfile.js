6b4f2f85337f8f73b34b9c63056025d9ee205314
Jakub Hampl <honitom@seznam.cz>
2015-05-07 15:34:08 +0100

    Fixup

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,7 +161,8 @@ module.exports = function(grunt) {
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
-        commitFiles: ['package.json', 'bower.json']
+        commitFiles: ['package.json', 'bower.json'],
+        push: false
       }
     },
   });
